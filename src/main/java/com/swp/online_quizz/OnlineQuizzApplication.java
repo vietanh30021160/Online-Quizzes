395b8c1830f4497b86f8a87f0655e658a00fb994
sonnld <130831645+sonnld@users.noreply.github.com>
2024-01-05 23:39:07 +0700

    init

---
+++ src/main/java/com/swp/online_quizz/OnlineQuizzApplication.java
@@ -0,0 +1,13 @@
+package com.swp.online_quizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQuizzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineQuizzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swp/online_quizz/OnlineQuizzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp.online_quizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQuizzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
