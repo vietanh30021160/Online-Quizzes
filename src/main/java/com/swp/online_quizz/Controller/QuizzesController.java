c28548a89a4f427db4a37c10c4281cf04c995c14
anhlvhe172692 <anhlvhe172692@fpt.edu.vn>
2024-01-28 19:46:13 +0700

    check null listAttempt

--- src/main/java/com/swp/online_quizz/Controller/QuizzesController.java
+++ src/main/java/com/swp/online_quizz/Controller/QuizzesController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.swp.online_quizz.Entity.Question;
-import com.swp.online_quizz.Entity.QuizAttempt;
 import com.swp.online_quizz.Entity.Quiz;
+import com.swp.online_quizz.Entity.QuizAttempt;
 import com.swp.online_quizz.Entity.User;
 import com.swp.online_quizz.Service.IQuizAttemptsService;
 import com.swp.online_quizz.Service.IQuizzesService;
@@ -32,7 +32,7 @@ public class QuizzesController {
 
     @GetMapping("/{quizID}")
     public String quizInfo(@PathVariable Integer quizID, HttpSession session, Model model) {
-        User user1 = iUsersService.getUsersByID(8);
+        User user1 = iUsersService.getUsersByID(13);
         Quiz quiz = iQuizzesService.getOneQuizz(quizID);
         List<QuizAttempt> listAttempts = iQuizAttemptsService.getAttemptByUserIdAndQuizzId(quiz, user1);
         model.addAttribute("listAttempts", listAttempts);
