63e8ff01152ba704ed14177d32a2f3f0fcd54f07
anhlvhe172692 <anhlvhe172692@fpt.edu.vn>
2024-02-24 21:25:01 +0700

    merge develop

--- src/main/java/com/swp/online_quizz/Repository/UsersRepository.java
+++ src/main/java/com/swp/online_quizz/Repository/UsersRepository.java
@@ -3,6 +3,7 @@ package com.swp.online_quizz.Repository;
 import java.util.List;
 import java.util.Optional;
 
+import com.swp.online_quizz.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
