e7213112ab0abccc1f47593848dc444f08f04422
VietAnh <113483743+vietanh30021160@users.noreply.github.com>
2024-03-10 15:55:41 +0700

    exception

--- src/main/java/com/swp/online_quizz/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/swp/online_quizz/Exception/GlobalExceptionHandler.java
@@ -7,5 +7,8 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    
+    @ExceptionHandler(value = Exception.class)
+    public String defaultErrorHandler(HttpServletRequest req, Exception e) {
+        return "notFoundQuiz";
+    }
 }
