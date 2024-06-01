648d2143e55aad67d21f29e5d3286b76f24c40e9
anhlvhe172692 <anhlvhe172692@fpt.edu.vn>
2024-06-01 19:05:00 +0700

    add QuizCompletionTask to system auto scan 1 second

---
+++ src/main/java/com/swp/online_quizz/Configuration/WebConfig.java
@@ -0,0 +1,15 @@
+package com.swp.online_quizz.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
--- src/main/java/com/swp/online_quizz/Repository/QuizAttemptsRepository.java
+++ src/main/java/com/swp/online_quizz/Repository/QuizAttemptsRepository.java
@@ -1,30 +1,35 @@
 package com.swp.online_quizz.Repository;
 
-import com.swp.online_quizz.Entity.QuizAttempt;
+import java.sql.Timestamp;
+import java.util.List;
+
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Timestamp;
-import java.util.List;
+import com.swp.online_quizz.Entity.QuizAttempt;
 
 @Repository
-public interface QuizAttemptsRepository extends JpaRepository<QuizAttempt,Integer> {
+public interface QuizAttemptsRepository extends JpaRepository<QuizAttempt, Integer> {
     List<QuizAttempt> findByQuizQuizId(Integer quizId);
+
     QuizAttempt findByAttemptId(Integer quizAttemptId);
+
     @Query(value = "SELECT qa FROM QuizAttempt qa WHERE qa.quiz.quizId = ?1")
     public List<QuizAttempt> findQuizAttemptsByQuizID(Integer quizID, Sort sort);
 
     @Query(value = "select qa from QuizAttempt qa join FETCH qa.user u join fetch qa.quiz q where u.username like %?1% and q.quizId = ?2")
     public List<QuizAttempt> searchUseByName(String username, Integer quizID);
+
     @Query("SELECT qa FROM QuizAttempt qa WHERE qa.quiz.quizId = :quizId AND qa.user.userId = :userId")
     List<QuizAttempt> findByQuizIdAndUserId(@Param("quizId") Integer quizId, @Param("userId") Integer userId);
 
     @Query("SELECT qa FROM QuizAttempt qa WHERE qa.quiz.quizId = :quizId AND qa.user.userId = :userId AND qa.startTime >= :startTime")
     List<QuizAttempt> findByQuizIdAndUserIdAndStartTime(@Param("quizId") Integer quizId,
-                                                        @Param("userId") Integer userId, @Param("startTime") Timestamp startTime);
+            @Param("userId") Integer userId, @Param("startTime") Timestamp startTime);
+
+    @Query("SELECT q FROM QuizAttempt q WHERE q.isComplete = false AND q.deadline < CURRENT_TIMESTAMP")
+    List<QuizAttempt> findExpiredIncompleteQuizzes();
 }
-
-
